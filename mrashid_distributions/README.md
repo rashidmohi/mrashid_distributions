eaa32b3fec2ef37727c8824896dbe37748e8b52a
Rashid Mohiuddin <mohiuddin.rashid@yahoo.com>
2022-12-24 18:58:11 +0800

    initial commit

---
+++ mrashid_distributions/README.md
@@ -0,0 +1,11 @@
+# mrashid-distributions package
+
+This package contains the Gaussian and Binomial class files
+
+# Files
+
+class files for Gaussian and Binomial distribution
+
+# Installation
+
+do the pip install
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mrashid_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mrashid_distributions'],
+      zip_safe=False)
